d43c2c391a58015ab8d26d9111c66a6f60fc9d5a
Emanuels <you@example.com>
2024-11-22 10:34:48 +0200

    first commit

---
+++ basics.js
@@ -0,0 +1,17 @@
+// let personAge = 17;
+// for (let i = 17; i > 17;) {
+
+    
+//     console.log(i)
+// }
+
+let namesArray = ["Jānis", "Anna", "Zaiga"];
+for (let i = 0; i < namesArray.length; i++){
+    
+
+    console.log(namesArray[i])
+}
+
+function sayHello(){
+    
+}
